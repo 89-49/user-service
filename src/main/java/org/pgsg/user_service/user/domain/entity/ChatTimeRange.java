1e1284bd7d07de3bab8caa25180438b1ed6be0c9
Hyeonbin2379 <hyeonbin2379@gmail.com>
2026-04-23 20:14:17 +0900

    [Feat] 회원 엔티티 및 VO 추가 (#4)
    * feat: 회원권한 vo 추가
    
    - 회원권한은 USER, MANAGER, MASTER로 구분
    - X-User-Role의 문자열에 해당하는 회원권한 enum을 찾아 반환하는 도메인 로직 추가
    
    * feat: 채팅가능시간대 vo 추가
    
    - 1개의 채팅가능 시간대는 요일, 시작시간, 종료시간 필드로 구성
    - VO 생성에 필요한 정적 팩토리 메서드 추가
    
    * feat: 회원 엔티티 추가
    
    - 회원 도메인의 애그리거트 루트 역할을 하는 엔티티
    - 회원의 username, name, nickname에 관한 복합 unique 제약조건 추가
    - 회원 엔티티 생성에 필요한 정적 팩토리 메서드 추가
    - 회원의 채팅가능시간대를 저장하는 도메인 로직은 인증 기능 구현 완료 흐 추가할 예정입니다.

---
+++ src/main/java/org/pgsg/user_service/user/domain/entity/ChatTimeRange.java
@@ -0,0 +1,33 @@
+package org.pgsg.user_service.user.domain.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ChatTimeRange {
+
+	@Column(name = "day_of_week", nullable = false)
+	private DayOfWeek dayOfWeek;
+
+	// 요일별 채팅가능 시간이므로 날짜 정보 없이 시간만 저장
+	@Column(name = "start_time", nullable = false)
+	private LocalTime startTime;
+
+	@Column(name = "end_time", nullable = false)
+	private LocalTime endTime;
+
+	public static ChatTimeRange of(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+		return new ChatTimeRange(dayOfWeek, startTime, endTime);
+	}
+}
---
+++ src/main/java/org/pgsg/user_service/user/domain/entity/UserRole.java
@@ -0,0 +1,21 @@
+package org.pgsg.user_service.user.domain.entity;
+
+import java.util.Arrays;
+
+public enum UserRole {
+
+	USER,
+	MANAGER,
+	MASTER;
+
+	public String getRole() {
+		return "ROLE_" + this.name();
+	}
+
+	public static UserRole find(String userRole) {
+		return Arrays.stream(UserRole.values())
+				.filter(role -> role.getRole().equals(userRole))
+				.findAny()
+				.orElseThrow(() -> new IllegalArgumentException("해당하는 회원권한을 찾을 수 없습니다."));	// 현재는 임시로 IllegalArgumentException 사용, 커스텀 예외 클래스 추가 시 교체 예정
+	}
+}
