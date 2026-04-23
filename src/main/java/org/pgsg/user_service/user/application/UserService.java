a70e2ca15a441a5142eeb5288a9a55b295203454
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-23 23:59:14 +0900

    feat: 회원 조회 서비스 로직 추가
    - application 패키지 내 UserService 클래스 추가 및 로그인용 회원 조회 기능 구현

---
+++ src/main/java/org/pgsg/user_service/user/application/UserService.java
@@ -0,0 +1,22 @@
+package org.pgsg.user_service.user.application;
+
+import lombok.RequiredArgsConstructor;
+import org.pgsg.user_service.user.application.dto.UserDetailsInfo;
+import org.pgsg.user_service.user.domain.entity.User;
+import org.pgsg.user_service.user.domain.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+	private final UserRepository userRepository;
+
+	public UserDetailsInfo getUser(String username) {
+		User user = userRepository.findByUsername(username)
+				// TODO: 회원 도메인용 커스텀 예외 클래스로 대체
+				.orElseThrow(() -> new IllegalArgumentException("해당 username을 사용하는 회원을 찾을 수 없습니다."));
+
+		return UserDetailsInfo.from(user);
+	}
+}
