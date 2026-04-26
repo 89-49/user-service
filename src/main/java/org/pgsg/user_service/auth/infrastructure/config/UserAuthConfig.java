360fa654c21b81c28827fcf7125b083cea25c77a
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-27 00:19:44 +0900

    refactor: accessToken의 Bearer 접두사 추가 시점 변경
    - 회원/인증 도메인 전용 security 설정에 관한 UserAuthConfig 추가
    - UserAuthConfig 설정과 공통 모듈의 SecurityConfig 설정을 모두 적용

--- src/main/java/org/pgsg/user_service/auth/infrastructure/config/UserAuthConfig.java
+++ src/main/java/org/pgsg/user_service/auth/infrastructure/config/UserAuthConfig.java
@@ -1,15 +1,38 @@
 package org.pgsg.user_service.auth.infrastructure.config;
 
+import lombok.RequiredArgsConstructor;
+import org.pgsg.config.security.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 // 로그인 인증에 필요한 추가적인 빈 등록
 @Configuration
-public class UserAuthConfig {
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Import(SecurityConfigImpl.class)
+public class UserAuthConfig implements SecurityConfig {
+
+    private final SecurityConfigImpl commonSecurityConfig;
+
+    @Bean
+    @Primary
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(auth -> auth
+            .requestMatchers("/api/v1/auth/**").permitAll()     // 로그인, 회원가입 허용
+        );
+
+        return commonSecurityConfig.filterChain(http);
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
