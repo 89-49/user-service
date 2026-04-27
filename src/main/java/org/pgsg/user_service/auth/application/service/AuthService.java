f07c3df6508c0fead26c7506cff250e39e93f22c
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-28 01:29:44 +0900

    fix: 코드래빗 수정사항 반영
    - docker-compose.yml 파일의 포트 매핑이 SERVER_PORT 환경 변수와 동기화되도록 수정
    - AuthService 내 하드코딩된 refreshtoken TTL 수정

--- src/main/java/org/pgsg/user_service/auth/application/service/AuthService.java
+++ src/main/java/org/pgsg/user_service/auth/application/service/AuthService.java
@@ -10,6 +10,7 @@ import org.pgsg.user_service.auth.domain.JwtTokenProvider;
 import org.pgsg.user_service.auth.domain.TokenRepository;
 import org.pgsg.user_service.auth.domain.UserAuthenticator;
 import org.pgsg.user_service.auth.domain.model.TokenPair;
+import org.pgsg.user_service.auth.infrastructure.JwtProperties;
 import org.pgsg.user_service.user.application.UserService;
 import org.pgsg.user_service.user.application.dto.command.CreateUserCommand;
 import org.pgsg.user_service.user.application.dto.info.UserDetailInfo;
@@ -24,6 +25,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class AuthService {
 
+    private final JwtProperties jwtProperties;
     private final UserService userService;  // 분리할 경우 내부적으로 FeignClient를 사용하는 UserProvider로 대체 필요
     private final JwtTokenProvider jwtTokenProvider;
     private final TokenRepository tokenRepository;
@@ -39,7 +41,11 @@ public class AuthService {
 
         // 토큰 생성 및 저장
         TokenPair tokenPair = jwtTokenProvider.createTokenPair(userDetail);
-        tokenRepository.saveRefreshToken(userDetail.getUuid(), tokenPair.getRefreshToken(), Duration.ofDays(7));
+        tokenRepository.saveRefreshToken(
+                userDetail.getUuid(),
+                tokenPair.getRefreshToken(),
+                Duration.ofMillis(jwtProperties.getRefreshTokenExpiration())
+        );
 
         return AuthInfo.from(tokenPair);
     }
