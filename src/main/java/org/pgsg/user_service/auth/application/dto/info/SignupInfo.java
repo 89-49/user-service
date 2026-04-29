b038ad8f48f426f8430400695a03efb58a2054b6
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-29 18:06:38 +0900

    refactor: TODO 주석 미반영 사항 수정
    - SignupInfo에 createdAt, createdBy 필드 추가

--- src/main/java/org/pgsg/user_service/auth/application/dto/info/SignupInfo.java
+++ src/main/java/org/pgsg/user_service/auth/application/dto/info/SignupInfo.java
@@ -2,21 +2,24 @@ package org.pgsg.user_service.auth.application.dto.info;
 
 import org.pgsg.user_service.user.application.dto.info.UserDetailInfo;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 // Auth 도메인 내에서 가입 완료 정보를 전달하기 위한 전용 Info DTO
 public record SignupInfo(
 		UUID userId,
 		String username,
-		String nickname
-		// TODO: 공통모듈 도입 이후 createdAt, createdBy 필드 추가
+		String nickname,
+		LocalDateTime createdAt,
+		UUID createdBy
 ) {
 	public static SignupInfo from(UserDetailInfo userInfo) {
 		return new SignupInfo(
 				userInfo.userId(),
 				userInfo.username(),
-				userInfo.nickname()
-				// TODO: 공통모듈 도입 이후 createdAt, createdBy 필드 추가
+				userInfo.nickname(),
+				userInfo.createdAt(),
+				userInfo.createdBy()
 		);
 	}
 }
