ca727feb1422b61d85cdafccc69abb0d0090df84
Hyeonbin2379 <hyeonbin2379@gmail.com>
2026-05-01 15:59:03 +0900

    [Feat] 회원 자신의 회원정보 조회 기능 구현 (#31)
    * feat: 로그인한 회원 자신의 정보 조회 기능 추가
    
    - GET /api/v1/users/me api 엔드포인트 추가
    
    * fix: TODO 주석 작업 진행
    
    - UserSignupResponse DTO 내 createdAt, createdBy 필드 추가

--- src/main/java/org/pgsg/user_service/auth/presentation/dto/response/UserSignupResponse.java
+++ src/main/java/org/pgsg/user_service/auth/presentation/dto/response/UserSignupResponse.java
@@ -2,20 +2,23 @@ package org.pgsg.user_service.auth.presentation.dto.response;
 
 import org.pgsg.user_service.auth.application.dto.info.SignupInfo;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record UserSignupResponse(
 		UUID userId,
 		String username,
-		String nickname
-		// TODO: 공통모듈 도입 이후 createdAt, createdBy 필드 추가
+		String nickname,
+		LocalDateTime createdAt,
+		UUID createdBy
 ) {
 	public static UserSignupResponse from(SignupInfo signupInfo) {
 		return new UserSignupResponse(
 				signupInfo.userId(),
 				signupInfo.username(),
-				signupInfo.nickname()
-				// TODO: 공통모듈 도입 이후 createdAt, createdBy 필드 추가
+				signupInfo.nickname(),
+				signupInfo.createdAt(),
+				signupInfo.createdBy()
 		);
 	}
 }
--- src/main/java/org/pgsg/user_service/user/presentation/UserController.java
+++ src/main/java/org/pgsg/user_service/user/presentation/UserController.java
@@ -1,9 +1,11 @@
 package org.pgsg.user_service.user.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.pgsg.config.security.UserDetailsImpl;
 import org.pgsg.user_service.user.application.UserService;
 import org.pgsg.user_service.user.application.dto.info.UserDetailInfo;
 import org.pgsg.user_service.user.presentation.dto.response.UserDetailResponse;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,13 @@ public class UserController {
 	@GetMapping("/{userId}")
 	public UserDetailResponse getUser(@PathVariable("userId") UUID userId) {
 		UserDetailInfo userDetailInfo = userService.getUserForAdmin(userId);
+
+		return UserDetailResponse.from(userDetailInfo);
+	}
+
+	@GetMapping("/me")
+	public UserDetailResponse getUser(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+		UserDetailInfo userDetailInfo = userService.getUser(userDetails.getUuid());
 
 		return UserDetailResponse.from(userDetailInfo);
 	}
