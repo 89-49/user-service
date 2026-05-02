8f52617f8590c812084078659cf7de10420e40f9
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-05-02 12:09:43 +0900

    feat: 회원 목록 조회/검색용 엔드포인트 추가
    - GET /api/v1/users 요청을 처리하기 위한 엔드포인트 추가

--- src/main/java/org/pgsg/user_service/user/presentation/UserController.java
+++ src/main/java/org/pgsg/user_service/user/presentation/UserController.java
@@ -4,12 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.pgsg.config.security.UserDetailsImpl;
 import org.pgsg.user_service.user.application.UserService;
 import org.pgsg.user_service.user.application.dto.info.UserDetailInfo;
+import org.pgsg.user_service.user.application.dto.result.UserSearchResult;
+import org.pgsg.user_service.user.presentation.dto.request.UserSearchRequest;
 import org.pgsg.user_service.user.presentation.dto.response.UserDetailResponse;
+import org.pgsg.user_service.user.presentation.dto.response.UserSearchResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -32,5 +36,14 @@ public class UserController {
 		UserDetailInfo userDetailInfo = userService.getUser(userDetails.getUuid());
 
 		return UserDetailResponse.from(userDetailInfo);
+	}
+
+	@GetMapping
+	public Page<UserSearchResponse> getUserList(
+			@ModelAttribute UserSearchRequest searchRequest,
+			@PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+		Page<UserSearchResult> userList = userService.getUserList(searchRequest.toQuery(), pageable);
+
+		return userList.map(UserSearchResponse::from);
 	}
 }
