e8a5e1c05686b9ace37dfc58f8212b04fe63b9f5
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-05-02 22:28:34 +0900

    feat: 회원정보 수정, 삭제 api 호출용 엔드포인트 추가
    - PATCH /api/v1/users/{userId}, PATCH /api/v1/users/me 호출용 엔드포인트 추가
    - DELETE /api/v1/users/{userId} 엔드포인트 추가

--- src/main/java/org/pgsg/user_service/user/presentation/UserController.java
+++ src/main/java/org/pgsg/user_service/user/presentation/UserController.java
@@ -1,14 +1,17 @@
 package org.pgsg.user_service.user.presentation;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.pgsg.config.security.UserDetailsImpl;
+import org.pgsg.user_service.user.application.UserCommandFacade;
 import org.pgsg.user_service.user.application.UserQueryFacade;
 import org.pgsg.user_service.user.application.dto.info.UserDetailInfo;
+import org.pgsg.user_service.user.application.dto.result.UserDeleteResult;
 import org.pgsg.user_service.user.application.dto.result.UserSearchResult;
+import org.pgsg.user_service.user.application.dto.result.UserUpdateResult;
 import org.pgsg.user_service.user.presentation.dto.request.UserSearchRequest;
-import org.pgsg.user_service.user.presentation.dto.response.UserDetailResponse;
-import org.pgsg.user_service.user.presentation.dto.response.UserPageResponse;
-import org.pgsg.user_service.user.presentation.dto.response.UserSearchResponse;
+import org.pgsg.user_service.user.presentation.dto.request.UserUpdateRequest;
+import org.pgsg.user_service.user.presentation.dto.response.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -24,6 +27,7 @@ import java.util.UUID;
 public class UserController {
 
 	private final UserQueryFacade userQueryFacade;
+	private final UserCommandFacade userCommandFacade;
 
 	@GetMapping("/{userId}")
 	public UserDetailResponse getUser(@PathVariable("userId") UUID userId) {
@@ -46,5 +50,32 @@ public class UserController {
 		Page<UserSearchResult> userList = userQueryFacade.getUserList(searchRequest.toQuery(), pageable);
 
 		return UserPageResponse.from(userList.map(UserSearchResponse::from));
+	}
+
+	@PatchMapping("/me")
+	public UserUpdateResponse updateUser(
+			@AuthenticationPrincipal UserDetailsImpl userDetails,
+			@Valid @RequestBody UserUpdateRequest updateRequest) {
+		UserUpdateResult updateResult = userCommandFacade
+				.updateUser(updateRequest.toCommand(userDetails.getUuid()));
+
+		return UserUpdateResponse.from(updateResult);
+	}
+
+	@PatchMapping("/{userId}")
+	public UserUpdateResponse updateUser(
+			@PathVariable UUID userId,
+			@Valid @RequestBody UserUpdateRequest updateRequest) {
+		UserUpdateResult updateResult
+				= userCommandFacade.updateUser(updateRequest.toCommand(userId));
+
+		return UserUpdateResponse.from(updateResult);
+	}
+
+	@DeleteMapping("/{userId}")
+	public UserDeleteResponse deleteUser(@PathVariable UUID userId) {
+		UserDeleteResult deleteResult = userCommandFacade.deleteUser(userId);
+
+		return UserDeleteResponse.from(deleteResult);
 	}
 }
