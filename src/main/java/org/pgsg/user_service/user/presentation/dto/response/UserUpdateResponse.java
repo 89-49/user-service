6f9a54fd070b4a95a77b7973e29dcb1f34b220f3
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-05-03 02:07:34 +0900

    fix: 회원정보 수정 관련 응답 DTO 필드 추가
    - UserUpdateResponse의 누락된 필드 추가

--- src/main/java/org/pgsg/user_service/user/presentation/dto/response/UserUpdateResponse.java
+++ src/main/java/org/pgsg/user_service/user/presentation/dto/response/UserUpdateResponse.java
@@ -2,12 +2,19 @@ package org.pgsg.user_service.user.presentation.dto.response;
 
 import org.pgsg.user_service.user.application.dto.result.UserUpdateResult;
 
-public record UserUpdateResponse(
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+public record UserUpdateResponse(
+		UUID userId,
+		LocalDateTime modifiedAt,
+		UUID modifiedBy
 ) {
 	public static UserUpdateResponse from(UserUpdateResult userUpdateResult) {
 		return new UserUpdateResponse(
-
+				userUpdateResult.userId(),
+				userUpdateResult.modifiedAt(),
+				userUpdateResult.modifiedBy()
 		);
 	}
 }
