532c7b01abdb8021656bacb6fd38d21535c04cbf
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-05-08 00:44:26 +0900

    refactor: DTO 클래스를 record로 변경
    - 별도의 설정 없이도 JSON으로 직렬화/역직렬화를 수행하기 위해 DTO 클래스를 record로 수정

--- src/main/java/org/pgsg/user_service/auth/presentation/dto/request/UserLoginRequest.java
+++ src/main/java/org/pgsg/user_service/auth/presentation/dto/request/UserLoginRequest.java
@@ -1,21 +1,14 @@
 package org.pgsg.user_service.auth.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.pgsg.user_service.auth.application.dto.command.LoginUserCommand;
 
-@Getter
-@NoArgsConstructor
-public class UserLoginRequest {
-    @NotBlank
-    private String username;
-
-    @NotBlank
-    private String password;
-
+public record UserLoginRequest(
+        @NotBlank
+        String username,
+        @NotBlank
+        String password
+) {
     public LoginUserCommand toCommand() {
         return new LoginUserCommand(username, password);
     }
--- src/main/java/org/pgsg/user_service/auth/presentation/dto/response/UserLoginResponse.java
+++ src/main/java/org/pgsg/user_service/auth/presentation/dto/response/UserLoginResponse.java
@@ -1,16 +1,12 @@
 package org.pgsg.user_service.auth.presentation.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.pgsg.user_service.auth.application.dto.info.AuthInfo;
 
-@Getter
-@AllArgsConstructor
-public class UserLoginResponse {
-    private String accessToken;
-    private String refreshToken;
-
+public record UserLoginResponse(
+    String accessToken,
+    String refreshToken
+) {
     public static UserLoginResponse from(AuthInfo info) {
-        return new UserLoginResponse(info.getAccessToken(), info.getRefreshToken());
+        return new UserLoginResponse(info.accessToken(), info.refreshToken());
     }
 }
