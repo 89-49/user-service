38fcd67cc5f85084ef152ab8c5cca11b926fe554
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-05-08 22:39:12 +0900

    feat: 지정한 날짜, 시간을 충족하는 채팅 가능 시간대 검색용 api 추가
    - 내부호출용 GET /internal/v1/{userId}/chat-availability api 추가

--- src/main/java/org/pgsg/user_service/user/presentation/UserInternalController.java
+++ src/main/java/org/pgsg/user_service/user/presentation/UserInternalController.java
@@ -1,13 +1,18 @@
 package org.pgsg.user_service.user.presentation;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.pgsg.user_service.user.application.UserQueryFacade;
+import org.pgsg.user_service.user.application.dto.info.ChatTimeRangeInfo;
 import org.pgsg.user_service.user.application.dto.info.LoginUserDetailInfo;
 import org.pgsg.user_service.user.application.dto.info.UserDetailInfo;
+import org.pgsg.user_service.user.presentation.dto.request.UserChatTimeSearchRequest;
+import org.pgsg.user_service.user.presentation.dto.response.UserChatTimeListResponse;
 import org.pgsg.user_service.user.presentation.dto.response.UserLoginResponse;
 import org.pgsg.user_service.user.presentation.dto.response.UserDetailResponse;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -29,5 +34,13 @@ public class UserInternalController {
 		UserDetailInfo userDetailInfo = userQueryFacade.getUser(userId);
 
 		return UserDetailResponse.from(userDetailInfo);
+	}
+
+	@GetMapping("/{userId}/chat-availability")
+	public UserChatTimeListResponse getAvailableChatTime(
+			@PathVariable("userId") UUID userId, @Valid @ModelAttribute UserChatTimeSearchRequest request) {
+		List<ChatTimeRangeInfo> chatTimeRangeInfoList = userQueryFacade.getAvailableChatTime(userId, request.toQuery());
+
+		return UserChatTimeListResponse.from(chatTimeRangeInfoList);
 	}
 }
