bea639f9c06d815b7ff63b9ce574955c7d9a2a23
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-05-09 04:03:09 +0900

    fix: 코드래빗 피드백 반영
    - INSERT_SQL에 ON CONFLICT DO NOTHING 구문을 추가함으로써 멱등성을 확보하고, 이미 저장된 데이터가 존재하는 경우에는 다음 데이터 추가 작업을 진행
    - CHECK_SQL의 하드코딩된 데이터를 limit문으로 대체

--- src/main/java/org/pgsg/user_service/user/infrastructure/loader/ChatTimeCsvLoader.java
+++ src/main/java/org/pgsg/user_service/user/infrastructure/loader/ChatTimeCsvLoader.java
@@ -2,18 +2,24 @@ package org.pgsg.user_service.user.infrastructure.loader;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class ChatTimeCsvLoader extends AbstractCsvLoader {
 
-	private static final String CHECK_SQL = "SELECT COUNT(*) FROM p_chat_time_range " +
-			"WHERE user_id = CAST('9107aa7c-a733-4514-8033-89e601a450ab' AS UUID) " +
-			"AND day_of_week = 'THURSDAY' AND start_time = CAST('14:00:00' AS TIME)";
+	private static final String CHECK_SQL = "SELECT COUNT(*) FROM p_chat_time_range LIMIT 1";
 	private static final String CSV_PATH = "csv/p_chat_time_range.csv";
-	private static final String INSERT_SQL = "INSERT INTO p_chat_time_range (user_id, day_of_week, start_time, end_time) VALUES (CAST(? AS UUID), ?, CAST(? AS TIME), CAST(? AS TIME))";
+	private static final String INSERT_SQL = "INSERT INTO p_chat_time_range (user_id, day_of_week, start_time, end_time) " +
+			"VALUES (CAST(? AS UUID), ?, CAST(? AS TIME), CAST(? AS TIME)) ON CONFLICT DO NOTHING";
 
 	public ChatTimeCsvLoader(JdbcTemplate jdbcTemplate) {
 		super(jdbcTemplate, CHECK_SQL, CSV_PATH, INSERT_SQL);
+	}
+
+	@Override
+	@Transactional
+	public void load() throws Exception {
+		super.load();
 	}
 
 	@Override
