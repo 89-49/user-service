6769a3ac50e4f2d2526b033c0287e0f70e92834b
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-05-08 22:42:24 +0900

    chore: 사용하지 않는 패키지 import문 삭제
    - @AuthenticationPrincipal이 @RequestHeader로 대체되면서 더 이상 사용하지 않는 import문 삭제

--- src/main/java/org/pgsg/user_service/user/presentation/UserController.java
+++ src/main/java/org/pgsg/user_service/user/presentation/UserController.java
@@ -2,7 +2,6 @@ package org.pgsg.user_service.user.presentation;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.pgsg.config.security.UserDetailsImpl;
 import org.pgsg.user_service.user.application.UserCommandFacade;
 import org.pgsg.user_service.user.application.UserQueryFacade;
 import org.pgsg.user_service.user.application.dto.info.UserDetailInfo;
@@ -17,7 +16,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
